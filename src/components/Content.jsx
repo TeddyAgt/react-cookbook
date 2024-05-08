bc82873fb51f6a9d320967f4d029b5fc78aaf3d4
TeddyAgt <teddy.aboim@gmail.com>
2024-05-08 10:04:51 +0200

    add like feature

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,18 +1,22 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
+import { data } from "../data/recipes";
 
 function Content() {
+  const recipes = data;
+
   return (
     <main className={`flex-fill container p-20`}>
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <section className={`${styles.contentCard} card p-20`}>
         <div className={styles.grid}>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+          {recipes.map((recipe) => (
+            <Recipe
+              key={recipe._id}
+              title={recipe.title}
+              image={recipe.image}
+            />
+          ))}
         </div>
       </section>
     </main>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,14 +1,26 @@
 import styles from "./Recipe.module.scss";
-import picture from "../assets/images/food-1.jpg";
+import { useState } from "react";
 
-function Recipe() {
+function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
   return (
-    <article className={styles.recipe}>
+    <article
+      onClick={handleClick}
+      className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={picture} alt="" />
+        <img
+          src={image}
+          alt=""
+        />
       </div>
-      <div className={`${styles.recipeTitle} flex-center`}>
-        <h3>Grillades</h3>
+      <div className={`${styles.recipeTitle} flex-center flex-column`}>
+        <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart ${liked ? "text-primary" : ""}`}></i>
       </div>
     </article>
   );
